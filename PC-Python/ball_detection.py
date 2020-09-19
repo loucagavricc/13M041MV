a32e8fd14548687bfda122d08c2fa437833ccc98
Luka Gavric <luka.gavric@bitgear.rs>
2020-09-19 22:42:54 +0200

    Added new algorithm for ball detection
    Second ball detection algorithm performs Hough's transformation
    and detects circle more precisely than first algorithm.

--- PC-Python/ball_detection.py
+++ PC-Python/ball_detection.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 #import serial
 from time import sleep
+import math
 #import threading
 
 ##def timerThread():
@@ -17,44 +18,62 @@ from time import sleep
 kernelOpen=np.ones((5,5))
 kernelClose=np.ones((20,20))
 cam= cv2.VideoCapture(1)
-#ser= serial.Serial('COM4',115200)
-while True:
-    
-    ret,img=cam.read(1)
-    img = cv2.medianBlur(img,(5))
-    img = img[20:380,120:480]
-    img=cv2.resize(img,(240,320))
-    imggray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    imggray = cv2.bitwise_not(imggray)
-    cv2.imshow('thrs',imggray)
-    cv2.waitKey(1)
-    retval, threshold = cv2.threshold(imggray,50,255,cv2.THRESH_BINARY)
-    cv2.imshow('thrs',threshold)
-    cv2.waitKey(1)
-    maskOpen=cv2.morphologyEx(threshold,cv2.MORPH_OPEN,kernelOpen)
-    cv2.imshow('maskOpen',maskOpen)
-    cv2.waitKey(1)
-    maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
-    cv2.imshow('maskClose',maskClose)
-    cv2.waitKey(1)
-    contours,hierarchy=cv2.findContours(maskClose,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 
-  
+#show image
+#rotate camera if needed
+#any button to resume
+while(True):
+    ret,img=cam.read(1)
+    cv2.imshow('img',img)
+    if(cv2.waitKey(1) != -1):
+        break
+
+cv2.destroyAllWindows()
+
+# Select ROI
+r = cv2.selectROI(img)
+cv2.destroyAllWindows()
+
+while True:
+    #open frame
+    ret,img=cam.read(1)
+    #blur
+    img = cv2.medianBlur(img,(5))
+    #crop
+    img = img[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
+    #convert to gray
+    imggray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    #invert
+    imggray = cv2.bitwise_not(imggray)
+    #find threshold
+    retval, threshold = cv2.threshold(imggray,50,255,cv2.THRESH_BINARY)
+    #morphological opening
+    maskOpen=cv2.morphologyEx(threshold,cv2.MORPH_OPEN,kernelOpen)
+    #morphological closing
+    maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+    #get and draw contours
+    contours,hierarchy=cv2.findContours(maskClose,
+                                        cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)  
     cv2.drawContours(img,contours,-1,(0,255,0),3)
-    
+    #check if ball on plate
     if len(contours)>0:
         cnt=contours[0]
+        #find circle parameters of the ball
         (x,y),radius=cv2.minEnclosingCircle(cnt)
         center=(int(x),int(y))
         radius=int(radius)
+        #draw circle
         cv2.circle(img,center,radius,(0,255,0),2)
+        label = str(int(y))+','+str(int(x))
+        cv2.putText(img, label, center,
+                    cv2.FONT_HERSHEY_PLAIN, 1.0, (200,100,100), 2);
 
         
-        if(x_q.full() == False):
-            x_q.put(x)
-            
-        if(y_q.full() == False):
-            y_q.put(y)
+##        if(x_q.full() == False):
+##            x_q.put(x)
+##            
+##        if(y_q.full() == False):
+##            y_q.put(y)
     
 ##        x=int(x)
 ##        print(x)
@@ -77,6 +96,7 @@ while True:
 ##        ser.write(str(e))
 ##        ser.write(str(
         
+    #show image with contours    
     cv2.imshow('img',img)
     if(cv2.waitKey(1) != -1):
         break
@@ -87,3 +107,5 @@ while True:
 ##t2 = threading.Thread(target = ballDetectThread, args =(q1,q2, )) 
 ##t1.start() 
 ##t2.start()
+
+cv2.destroyAllWindows()
