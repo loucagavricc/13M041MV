8980de4bd9d5d320c8bd83213ed148e36a240b07
Luka Gavric <luka.gavric@bitgear.rs>
2020-09-19 23:39:04 +0200

    Ball detection improved

--- PC-Python/ball_detection_2.py
+++ PC-Python/ball_detection_2.py
@@ -1,21 +1,42 @@
 import cv2 
 import numpy as np
+import time
+import math
 
 cam= cv2.VideoCapture(1)
 
 while(True):
     ret,img=cam.read(1)
-    cv2.imshow('img',img)
+    cv2.imshow('Make plate parallel to image edge',img)
     if(cv2.waitKey(1) != -1):
         break
 
 cv2.destroyAllWindows()
 
 # Select ROI
-roi = cv2.selectROI(img)
+roi = cv2.selectROI("ROI select",img)
 cv2.destroyAllWindows()
 
+# Crop
+img = img[int(roi[1]):int(roi[1]+roi[3]),
+          int(roi[0]):int(roi[0]+roi[2])] 
+
+# Select desired ballance point
+bPointReg = cv2.selectROI("Balance point select",img)
+cv2.destroyAllWindows()
+
+bPointX = int(bPointReg[0] + bPointReg[2]/2)
+bPointY = int(bPointReg[1] + bPointReg[3]/2)
+
+bPoint = (bPointX, bPointY)
+
+leave = False
+
 while True:
+    if leave:
+        cv2.destroyAllWindows()
+        break
+    
     # Open frame
     ret,img=cam.read(1)
     
@@ -26,13 +47,13 @@ while True:
     img = img[int(roi[1]):int(roi[1]+roi[3]),
               int(roi[0]):int(roi[0]+roi[2])]
     
-    # Convert to grayscale. 
+    # Convert to grayscale 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
       
-    # Blur using 3 * 3 kernel. 
+    # Blur using 3 * 3 kernel
     gray_blurred = cv2.blur(gray, (3, 3)) 
       
-    # Apply Hough transform on the blurred image. 
+    # Apply Hough transform on the blurred image 
     detected_circles = cv2.HoughCircles(gray_blurred,  
                        cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, 
                    param2 = 30, minRadius = 1, maxRadius = 40) 
@@ -40,17 +61,25 @@ while True:
     # Draw circles that are detected. 
     if detected_circles is not None: 
       
-        # Convert the circle parameters a, b and r to integers. 
-        detected_circles = np.uint16(np.around(detected_circles)) 
-      
-        for pt in detected_circles[0, :]: 
-            a, b, r = pt[0], pt[1], pt[2] 
-      
-            # Draw the circumference of the circle. 
-            cv2.circle(img, (a, b), r, (0, 255, 0), 2) 
-      
-            # Draw a small circle (of radius 1) to show the center. 
-            cv2.circle(img, (a, b), 1, (0, 0, 255), 3) 
-            cv2.imshow("Detected Circle", img) 
-            if(cv2.waitKey(1) != -1):
-                break
+        # Convert the circle parameters a, b and r to integers 
+        detected_circles = np.uint16(np.around(detected_circles))
+
+        # Extract center and circumference of detected circle
+        a,b,r = detected_circles[0][0]
+        
+        # Draw the circumference of the circle 
+        cv2.circle(img, (a, b), r, (100, 255, 100), 2)
+  
+        # Draw a small circle (of radius 1) to show the center
+        cv2.circle(img, (a, b), 1, (100, 100, 255), 3)
+
+        # Draw a small circle (of radius 1) to show the ballancing point
+        cv2.circle(img, bPoint, 1, (100, 100, 255), 3)
+
+        # Draw error line from desired ballance point
+        cv2.line(img, (a, b), bPoint, (255, 100, 100), 1)
+
+        
+        cv2.imshow("Detected Circle", img) 
+        if(cv2.waitKey(1) != -1):
+            leave = True
